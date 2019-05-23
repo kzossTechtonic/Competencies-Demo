53dea62f00332dded326468841f56f2a08f914c9
karenna <karenna.zoss@techtonic.com>
2019-05-22 21:04:06 -0600

    Added rows to the database including the data and specific parameters to be inserted

---
+++ insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO songs (id, artist, title, year)
+VALUES (1, 'Fugees', 'Killing Me Softly', 1996);
+
+INSERT INTO songs (id, artist, title, year)
+VALUES (2, 'Bone Thugs N Harmony', 'Crossroads', 1995);
+
+INSERT INTO songs (id, artist, title, year)
+VALUES (3, 'Third Eye Blind', 'Semi-Charmed Life', 1997);
+
+INSERT INTO songs (id, artist, title, year)
+VALUES (4, 'The Spice Girls', 'Wannabe', 1996);
+
+INSERT INTO songs (id, artist, title, year)
+VALUES (5, 'TLC', 'No Scrubs', 1999);
+
+INSERT INTO songs (id, artist, title, year)
