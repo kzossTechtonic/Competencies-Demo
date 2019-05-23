ad1722ee1b9da5f985761f75578fbb221feee221
karenna <karenna.zoss@techtonic.com>
2019-05-23 09:39:52 -0600

    Added WHERE and AS to select queries

--- select.sql
+++ select.sql
@@ -6,3 +6,13 @@ SELECT `year` FROM `comp_schema`.`Songs`;
 
 SELECT `artist`, `title` FROM `comp_schema`.`Songs`;
 SELECT `title`, `year` FROM `comp_schema`.`Songs`;
+
+SELECT DISTINCT `year` FROM `comp_schema`.`Songs`;
+SELECT DISTINCT `artist` FROM `comp_schema`.`Songs`;
+
+SELECT * FROM `comp_schema`.`Songs` WHERE `year` > 1991;
+SELECT * FROM `comp_schema`.`Songs` WHERE `year` < 1999;
+SELECT * FROM `comp_schema`.`Songs` WHERE `year` = 1996;
+
+SELECT `artist` AS 'Names' FROM `comp_schema`.`Songs`;
+SELECT `title` AS '90s Hits' FROM `comp_schema`.`Songs`;
