8d19ae58114d30a88ae47841fb2ac464c9eaf78c
karenna <karenna.zoss@techtonic.com>
2019-05-23 09:48:28 -0600

    Created another LIKE operator

--- like.sql
+++ like.sql
@@ -1,1 +1,2 @@
-SELECT * FROM `comp_schema`.`Songs` WHERE `year` LIKE '199_';
+SELECT * FROM `comp_schema`.`Songs` WHERE `year` LIKE `199_`;
+SELECT * FROM `comp_schema`.`Songs` WHERE `artist` LIKE `T&`;
