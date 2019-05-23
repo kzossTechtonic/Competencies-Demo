0b81b8d92b37b85add2b1a8fa8581f1be0eb21c2
karenna <karenna.zoss@techtonic.com>
2019-05-22 23:00:41 -0600

    Updated the create statements

--- create.sql
+++ create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `comp_schema`.`Songs`(
-    id INTEGER,
-    artist TEXT,
-    title TEXT,
-    year INTEGER
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `artist` varchar(45) NOT NULL,
+    `title` varchar(45) NOT NULL,
+    `year` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
 );
