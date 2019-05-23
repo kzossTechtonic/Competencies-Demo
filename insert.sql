2f1201853e1b59b0580277daaed74df90e3a0cbf
karenna <karenna.zoss@techtonic.com>
2019-05-22 22:18:33 -0600

    Inserted values into workbench

--- insert.sql
+++ insert.sql
@@ -1,38 +1,91 @@
-INSERT INTO songs (id, artist, title, year)
-VALUES (1, 'Fugees', 'Killing Me Softly', 1996);
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
+VALUES
+(1, 'Fugees', 'Killing Me Softly', 1996);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (2, 'Bone Thugs N Harmony', 'Crossroads', 1995);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (3, 'Third Eye Blind', 'Semi-Charmed Life', 1997);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (4, 'The Spice Girls', 'Wannabe', 1996);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (5, 'TLC', 'No Scrubs', 1999);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (6, 'Blackstreet', 'No Diggity', 1996);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (7, "Coolio", "Gangsta's Paradise", 1995);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (8, 'Nirvana', 'Smells Like Teen Spirit', 1991);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (9, "Dr. Dre", "Nothin' But a G Thang", 1992);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (10, 'Mariah Carey', 'Vision of Love', 1990);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (11, 'Beastie Boys', 'Sabotage', 1994);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (12, 'Salt-N-Pepa', 'Whatta Man', 1993);
 
-INSERT INTO songs (id, artist, title, year)
+INSERT INTO `comp_schema`.`Songs`
+(`id`,
+`artist`,
+`title`,
+`year`)
 VALUES (13, "NSYNC", "Tearin' Up My Heart", 1998);
